75287b3d9e0897f3d98b62d8c5bf038a6ae15845
max.knutsson+demo@dbtlabs.com <max.knutsson+demo@dbtlabs.com>
2024-05-19 08:58:36 +0000

    refactor to cleanup and calc ltv

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,23 +1,30 @@
-with payments as (
-        select
-            payment_id,
-            order_id,
-            amount
-        from {{ ref("stg_payments") }}
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
-orders as (
-    select * from {{ ref('stg_orders') }}
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
 ),
 
 final as (
+
     select
         orders.order_id,
         orders.customer_id,
-        payments.amount
-    from orders
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
 
-    left join payments using (order_id)
+    from orders
+    left join order_payments using (order_id)
 )
 
 select * from final
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,8 +1,11 @@
 select
     id as payment_id,
     orderid as order_id,
-    created,
+    paymentmethod as payment_method,
     status,
-    amount
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
 
 from {{ source('stripe', 'payment') }}
