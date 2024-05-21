30d298a645691e68af5dfdc9dad811740daee71c
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-05-21 14:41:56 +0000

    new model test

--- models/DEMO/dim_customers_demo.sql
+++ models/DEMO/dim_customers_demo.sql
@@ -12,7 +12,11 @@ customer_orders as (
     select
         c.customer_id,
         c.customer_name,
-        sum(o.order_total) as total_order_value
+        sum(o.order_total) as total_order_value,
+        case 
+            when total_order_value > 75 then 'gold'
+            else 'normal'
+        end as customer_type
     from customers c
     left join orders o on c.customer_id = o.customer_id
     group by c.customer_id, c.customer_name
