0afe703c95e7e2bf289106f5c382d037d89a1c32
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-05-27 12:40:56 +0000

    commit for snowflake setup

--- models/staging/jaffle_shop/stg_order_items.sql
+++ models/staging/jaffle_shop/stg_order_items.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('jaffle_shop', 'items') }}
+    select * from {{ source('jaffle_shop', 'raw_items') }}
 
 ),
 
--- models/template/stg_customers_tmp.sql
+++ models/template/stg_customers_tmp.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     name as customer_name
 
-from raw.jaffle_shop.customers
+from raw_jaffle_shop.raw_customers
