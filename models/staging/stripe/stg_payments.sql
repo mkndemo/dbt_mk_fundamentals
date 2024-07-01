e70c9cbcb347a8cf29c41c0957eafb1ce7c4657b
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-07-01 13:16:07 +0000

    incremental model examples

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,30 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
+{{
+    config(
+        materialized='incremental'
+    )
+}}
 
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+with events as (
+    select * 
+    from {{ source('stripe', 'raw_payment') }}
 
-from {{ source('stripe', 'raw_payment') }}
+    {% if is_incremental() %}
+    where _batched_at >= (select top 1 _batched_at from {{this}} order by _batched_at desc )
+    {% endif %}
+),
+
+final as (
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at,
+        _batched_at
+
+    from events
+)
+select * from final
