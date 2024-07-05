45a7ef1e27af7ec6b2ee3eef98dc5b7c36726da7
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-07-05 14:15:17 +0000

    commit

--- macros/dynamic_column.sql
+++ macros/dynamic_column.sql
@@ -1,7 +1,7 @@
 {% macro my_dynamic_column(column_name) %}
     CASE
         WHEN {{ column_name }} = 'success' THEN 'OK'
-        WHEN {{ column_name }} = 'fail' THEN 'notok'
+        WHEN {{ column_name }} = 'fail' THEN 'notok1'
         ELSE 'other'
     END
 {% endmacro %}
